67429e66a8e11d383e1e56db802631118473c174
izjing <53531436@qq.com>
2020-04-08 15:50:13 +0800

    feat: 引入登陆鉴权，添加接口

--- app/router.js
+++ app/router.js
@@ -5,10 +5,19 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  // const jwt = app.middleware.jwt(app.config.jwt);
+  const jwt = app.middleware.jwt(app.config.jwt);
   // router.get('/', controller.home.index);
   router.post('/signup', controller.home.signup);
   router.post('/login', controller.home.login);
   router.post('/wenzhang', controller.wenzhang.wenzhangup);
   router.get('/findwenzhang', controller.wenzhang.findwenzhang);
+  router.get('/findOneArticle', controller.wenzhang.findOneArticle);
+  router.post('/dianzan', jwt, controller.dianzan.dianzanup);
+  router.post('/finddianzan', controller.dianzan.finddianzan);
+  // 添加说说
+  router.post('/addAbout', jwt, controller.about.addAbout);
+  router.post('/findAbout', controller.about.findAbout);
+  // 添加留言
+  router.post('/addMb', jwt, controller.messageBoard.addMb);
+  router.post('/findMb', controller.messageBoard.findMb);
 };
