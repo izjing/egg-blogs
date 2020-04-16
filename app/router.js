cec2e35f0f0feb882f91b480b99d78666df22b6b
izjing <53531436@qq.com>
2020-04-16 23:59:12 +0800

    style: 接口增加api

--- app/router.js
+++ app/router.js
@@ -7,20 +7,21 @@ module.exports = app => {
   const { router, controller } = app;
   const jwt = app.middleware.jwt(app.config.jwt);
   // router.get('/', controller.home.index);
-  router.post('/signup', controller.home.signup);
-  router.post('/login', controller.home.login);
-  router.post('/loginMail', controller.home.loginMail);
-  router.post('/wenzhang', controller.wenzhang.wenzhangup);
-  router.get('/findwenzhang', controller.wenzhang.findwenzhang);
-  router.get('/findOneArticle', controller.wenzhang.findOneArticle);
+  router.post('/api/api/signup', controller.home.signup);
+  router.post('/api/login', controller.home.login);
+  router.post('/api/loginMail', controller.home.loginMail);
+  // 文章
+  router.post('/api/wenzhang', jwt, controller.wenzhang.wenzhangup);
+  router.get('/api/findwenzhang', controller.wenzhang.findwenzhang);
+  router.get('/api/findOneArticle', controller.wenzhang.findOneArticle);
   // 点赞
-  router.post('/dianzan', jwt, controller.dianzan.dianzanup);
-  router.post('/finddianzan', controller.dianzan.finddianzan);
-  router.get('/findAllZan', controller.dianzan.findAllZan);
+  router.post('/api/dianzan', jwt, controller.dianzan.dianzanup);
+  router.post('/api/finddianzan', controller.dianzan.finddianzan);
+  router.get('/api/findAllZan', controller.dianzan.findAllZan);
   // 添加说说
-  router.post('/addAbout', jwt, controller.about.addAbout);
-  router.post('/findAbout', controller.about.findAbout);
+  router.post('/api/addAbout', jwt, controller.about.addAbout);
+  router.post('/api/findAbout', controller.about.findAbout);
   // 添加留言
-  router.post('/addMb', jwt, controller.messageBoard.addMb);
-  router.post('/findMb', controller.messageBoard.findMb);
+  router.post('/api/addMb', jwt, controller.messageBoard.addMb);
+  router.post('/api/findMb', controller.messageBoard.findMb);
 };
